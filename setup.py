81a6268ef01f195942595ddc366d53dc84cdc761
Peter Bingham <petersbingham@hotmail.co.uk>
2018-03-17 11:04:42 +0000

    Copy test file and README on installation.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import shutil
+shutil.copy('README.md', 'matfuncutil/README.md')
 
 setup(name='matfuncutil',
-      version='0.7',
+      version='0.8',
       description='Containers for functional data in discrete and continuous forms.',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
       packages=['matfuncutil'],
+      package_data={'matfuncutil': ['tests/*', 'README.md'],
      )
