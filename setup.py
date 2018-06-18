2782dc09fc94f7a9e30dcd7f6c825208aa0705c6
Peter Bingham <petersbingham@hotmail.co.uk>
2018-06-18 22:34:26 +0100

    Set version number for release.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import shutil
 shutil.copy('README.md', 'matfuncutil/README.md')
 
 setup(name='matfuncutil',
-      version='0.41',
+      version='1.0.0',
       description='Containers for functional data in discrete and continuous forms.',
       author="Peter Bingham",
       author_email="petersbingham@hotmail.co.uk",
