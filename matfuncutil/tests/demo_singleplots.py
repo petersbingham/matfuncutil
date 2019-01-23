2eae3b868f4f723e455ce9cb59745f5ccb99e7b1
Peter Bingham <petersbingham@hotmail.co.uk>
2019-01-23 21:05:37 +0000

    Add additional chart configuration options.

--- matfuncutil/tests/demo_singleplots.py
+++ matfuncutil/tests/demo_singleplots.py
@@ -12,15 +12,25 @@ os.makedirs("charts")
 
 a = dum_mats.row_offset_col_gain_zeroimag()
 a.set_chart_title("Test title a")
+a.plot()
+a.set_chart_parameters(dashes=[None,[10,10],[50,50]])
 a.plot(save_path=basedir+os.sep+"charts\\chart.png", add_axis_lmts=True)
+
+a.set_chart_parameters(dashes=[[10,10]])
+a.plot(add_axis_lmts=True)
+
+a.set_chart_parameters(dashes=[[10,10]], cycle_dashes=True)
+a.plot(add_axis_lmts=True)
 
 b = a.create_reduced_dim(0)
 b.set_chart_title("Test title b")
-b.plot(logx=True, logy=True)
+b.set_chart_parameters(xsize=4., ysize=4., dpi=2400)
+b.plot(logx=True, logy=True, save_path=basedir+os.sep+"charts\\chart2.png")
 
 c = b.create_reduced_dim(1)
+c.set_chart_title(None)
 c.set_chart_parameters(leg_prefix="Test", xsize=10, ysize=10)
-c.plot(imag=True)
+c.plot(imag=True, save_path=basedir+os.sep+"charts\\chart3.pdf")
 
 d = a.trace()
 d.plot()
@@ -32,4 +42,7 @@ dSin.set_axis_labels("Ax", "Ay")
 dSin.plot()
 
 dSin2 = dSin.create_reduced_length(-math.pi, math.pi, 20)
+dSin2.plot()
+
+dSin2.set_chart_parameters(dashes=[[50,50]])
 dSin2.plot()
